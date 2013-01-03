49f6a9d7832ef4fc09ab2234e077d1744f922cfd
Rob Gordon <suffolksoft@gmail.com>
2013-01-03 13:02:38 -0600

    TDD Seesion 3 (amemdment)

--- cs325/src/assignments/monopoly/tests/DieTests.java
+++ cs325/src/assignments/monopoly/tests/DieTests.java
@@ -9,7 +9,7 @@ import assignments.monopoly.Die;
 public class DieTests {
 
 	@Test
-	public void ValueShouldBeBetweenOneAndSix() {
+	public void () {
 		Die d = new Die();
 		for (int i = 0; i < 1000; i++) {
 			int value = d.roll();
