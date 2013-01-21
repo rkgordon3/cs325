8470130d4d660018577f35bba9e359a6e1e051a0
Rob Gordon <suffolksoft@gmail.com>
2013-01-21 10:28:05 -0600

    Safe I0-2

--- cs325/src/assignments/safe/Safe.java
+++ cs325/src/assignments/safe/Safe.java
@@ -1,5 +1,14 @@
 package assignments.safe;
 
-public class Safe {
+public interface Safe {
+
+	public static final String BLANK_DISPLAY = "      ";
+	public static final String OPEN_DISPLAY = " OPEN ";
+
+	public String readDisplay();
+
+	public boolean isLocked();
+
+	public void enter(char c);
 
 }
